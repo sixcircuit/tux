cc206799580cac2a6fe708dca6c4b01b71a8270d
Kendrick Taylor <kendrick@circuitsix.com>
2014-11-23 17:26:45 -0800

    support .tux.js config file, update readme, add shrinkwrap

---
+++ tux.js
@@ -0,0 +1,13 @@
+function home(){ return("~"); }
+
+exports.config = {
+    "foo" : { // session name
+        cwd: home() // all your new windows will start here
+        windows: [ // the windows will be created in this order, the commands will be run
+            { cmd: "ls" }, 
+            { cmd: "vi" }
+            { cmd: "cd /bin && ls"}
+        ]
+    }
+}
+`
